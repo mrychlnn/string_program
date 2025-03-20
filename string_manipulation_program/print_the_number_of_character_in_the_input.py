df8ec22170cfd34d7671e685833eec7a6d93698b
Chloe <lunamarychloeanne@gmail.com>
2025-03-20 15:30:22 +0800

    print output

--- string_manipulation_program/print_the_number_of_character_in_the_input.py
+++ string_manipulation_program/print_the_number_of_character_in_the_input.py
@@ -1,4 +1,8 @@
 # Input the users full name
 full_name = input("Enter your full name: ")
 
+# Count the characters
+character_count = len(full_name)
+
 # Print the number of character in the given full name
+print("output:", character_count)
