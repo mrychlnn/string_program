8b0356be8a0d7bd5588f609092293cd375c874d9
Chloe <lunamarychloeanne@gmail.com>
2025-03-20 15:20:10 +0800

    print output

--- string_manipulation_program/print_the_number_of_words_input.py
+++ string_manipulation_program/print_the_number_of_words_input.py
@@ -1,4 +1,8 @@
 # Input the words
 words = input("Enter a statement: ")
 
+# Count the words
+word_count = len(words.split())
+
 # Print the number of words in the input
+print("Output:", word_count)
