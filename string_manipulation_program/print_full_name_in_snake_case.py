ce220fb1fc615235bb64d5b9beaac6fc8ed9e611
Chloe <lunamarychloeanne@gmail.com>
2025-03-20 15:46:26 +0800

    print output

--- string_manipulation_program/print_full_name_in_snake_case.py
+++ string_manipulation_program/print_full_name_in_snake_case.py
@@ -1,4 +1,8 @@
 # Input the users full name
 full_name = input("Enter your full name: ")
 
+# Convert in snake casing
+snake_case_name = full_name.lower().replace(" ", "_")
+
 # Print the full name in snake case
+print("Output:", snake_case_name)
