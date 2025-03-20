de8b2a135c9ab358074aaa624b7f749b94a1b3bf
Chloe <lunamarychloeanne@gmail.com>
2025-03-20 14:29:37 +0800

    print output

--- string_manipulation_program/print_full_name_without_spaces_in_beginning.py
+++ string_manipulation_program/print_full_name_without_spaces_in_beginning.py
@@ -1,4 +1,8 @@
 # Input the users full name
 full_name = input("Enter your full name: ")
 
+# Remove leading spaces
+remove_spaces = full_name.lstrip()
+
 # Print the output w/o spaces in the beginning
+print("Output:", remove_spaces)
