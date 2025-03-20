d59af5cba61a2095852c602320283afd9feaeb4e
Chloe <lunamarychloeanne@gmail.com>
2025-03-20 15:39:22 +0800

    print output

--- string_manipulation_program/print_full_name_in_pascal_case.py
+++ string_manipulation_program/print_full_name_in_pascal_case.py
@@ -1,4 +1,8 @@
 # Input the users full name
 full_name = input("Enter your full name: ")
 
+# Convert name in Pascal case
+pascal_case_name = full_name.title().replace(" ", "")
+
 # Print the full name in pascal casing
+print("Output:", pascal_case_name)
